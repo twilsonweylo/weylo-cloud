b542db5ea05318e9cce273a94c0947085863bb88
twilsonweylo <twilson@weylo.ai>
2025-09-04 22:48:18 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "🚀 Weylo Cloud is live!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
